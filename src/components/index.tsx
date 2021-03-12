bf5ebd9cdf3009f9ad632f0c251e66f47374969d
kidow <wcgo2ling@gmail.com>
2021-03-12 22:55:15 +0900

    SEO update

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,7 @@
 export { default as ReHeader } from './Header'
 export { default as ReCard } from './Card'
 export { default as ReFooter } from './Footer'
+export { default as ReSEO } from './SEO'
 
 // Icons
 export { default as ReLogoIcon } from './Icons/Logo'
