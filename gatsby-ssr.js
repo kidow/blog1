293a40bfa3644359a8be143a384f9f8f82f1d072
kidow <wcgo2ling@gmail.com>
2021-05-21 17:18:30 +0900

    plugin-key 수정

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,7 +5,7 @@ export const onRenderBody = ({ setPostBodyComponents }) => {
     <script
       src="https://cdn-test-kohl.vercel.app/plugin.js"
       async
-      plugin-key="383feb63-4522-412d-bac9-42d0d877f0ac"
+      plugin-key="96b4cd7f-c21c-4328-9658-95c1a7f8566a"
     ></script>
   ])
 }
