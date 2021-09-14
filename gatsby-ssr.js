6df0af8fab50cfb36d675693388c7d72ef01146c
kidow <wcgo2ling@gmail.com>
2021-09-14 23:45:59 +0900

    feedbank 보류

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 export const onRenderBody = ({ setPostBodyComponents }) => {
-  setPostBodyComponents([
-    <script
-      src="https://cdn-test-kohl.vercel.app/plugin.js"
-      async
-      plugin-key="96b4cd7f-c21c-4328-9658-95c1a7f8566a"
-    ></script>
-  ])
+  // setPostBodyComponents([
+  //   <script
+  //     src="https://cdn-test-kohl.vercel.app/plugin.js"
+  //     async
+  //     plugin-key="96b4cd7f-c21c-4328-9658-95c1a7f8566a"
+  //   ></script>
+  // ])
 }
